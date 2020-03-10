3e999d5408ed794450e09d777f563aececc5979e
Tyler Lai <lai.t.tyler@gmail.com>
2020-03-10 14:19:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,8 @@
 class Dog
-  
-  def initialize(fido, snoopy, lassie)
-    @fido = fido
-    @snoopy = snoopy
-    @lassie = lassie
-  end
-  
+
 end
+
+fido = Dog.new 
+snoopy = Dog.new
+lassie = Dog.new 
 
